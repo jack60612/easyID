19cbaaa87c3b8f7f1f64db2ae58f59aaf99dc60b
Jack Nelson <jack@jacknelson.xyz>
2023-04-19 15:11:21 -0400

    add new api key

--- easyID/settings.py
+++ easyID/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 UNIDENTIFIED_SUBJECTS_TIMEOUT = 5  # seconds
 SIMILARITY_THRESHOLD = 0.8  # 80% similarity
 NUMBER_OF_SUBJECTS = 5  # number of subjects to be recognized
-DETECTION_PROBABILITY_THRESHOLD = 0.95  # 95% probability of a face
+DETECTION_PROBABILITY_THRESHOLD = 0.90  # 90% probability of a face
 DEFAULT_DIRECTORY = Path.home() / "easyID"
 # config options
 CF_OPTIONS = {
@@ -21,7 +21,7 @@ WEBCAM_HEIGHT = 720
 
 DEFAULT_HOST = "https://easyid-server.local"
 DEFAULT_PORT = "443"
-API_KEY = "9886bc85-44dc-4345-8e61-3b39a2a2dc42"
+API_KEY = "353d2674-467b-42fc-a754-d85fe6359a7c"
 # SELF_SIGNED_CERT_DIR = None # Default off
 SELF_SIGNED_CERT_DIR = Path("../easyID-server/main-nginx/nginx-selfsigned.crt")
 ADD_TIMESTAMP = True
